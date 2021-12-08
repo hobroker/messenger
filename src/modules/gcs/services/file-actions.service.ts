0946347bec7408cd95a114a70bbca3ed18b3cb9b
Igor <igor.leahu24@gmail.com>
2021-12-08 18:50:36 +0200

    Add decrement action

--- src/modules/gcs/services/file-actions.service.ts
+++ src/modules/gcs/services/file-actions.service.ts
@@ -9,10 +9,18 @@ export class FileActionsService {
     return toString(number + 1);
   }
 
+  decrement(raw: string) {
+    const number = Number(raw);
+
+    return toString(number + 1);
+  }
+
   handle(action: string, raw: string) {
     switch (action) {
       case 'increment':
         return this.increment(raw);
+      case 'decrement':
+        return this.decrement(raw);
       default:
         throw new Error(`Action ${action} not supported`);
     }
